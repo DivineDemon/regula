37b7f263afe6818de8844f29066e3f9b73604399
DivineDemon <supame123@gmail.com>
2025-12-21 12:21:30 +0500

    Cleanup.

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import * as React from "react";
+import type * as React from "react";
 
 import { cn } from "@/lib/utils";
 
 function Label({ className, ...props }: React.ComponentProps<"label">) {
   return (
+    // biome-ignore lint/a11y/noLabelWithoutControl: This is a reusable component that will be used with htmlFor or by wrapping inputs
     <label
       data-slot="label"
       className={cn(
